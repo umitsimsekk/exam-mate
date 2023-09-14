486e9bedf0c523fbf3adc715d30d5cdf25ded0c0
Ümit Şimşek <umitsimsek002@gmail.com>
2023-09-14 10:14:54 +0300

    Initial Commit

---
+++ ExamMate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExamMate
+//
+//  Created by Ümit Şimşek on 14.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
